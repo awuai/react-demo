fa9b5567cae7e41a344c2b8d29fbbaf06a98125f
awuai <873636145@qq.com>
2023-02-26 11:23:57 +0800

    清除多余代码；class组件初体验

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        react
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         react
+//       </header>
+//     </div>
+//   );
+// }
+class App extends React.Component {
+  state = { mun: 1 }
+  render() { 
+    const { mun } = this.state
+    const add = ()=>{
+      return this.setState({
+        mun:mun+1
+      })
+    }
+    return ( 
+      <div>
+        {mun}
+        <br/>
+        <button onClick={ ()=>{
+          add()
+        }}>🏠</button>
+      </div>
+     );
+  }
 }
 
 export default App;
